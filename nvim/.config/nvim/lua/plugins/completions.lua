1665499f7077664411a300b80bf73048dd7c5c74
abdellaouiamir <abdellaouiamir27@gmail.com>
2025-01-23 11:49:19 +0100

    updates

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -1,17 +1,19 @@
 return {
   {
-    "hrsh7th/cmp-nvim-lsp"
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets"
-    }
-  },
-  {
     "hrsh7th/nvim-cmp",
     version = false,
+    dependencies = {
+      "hrsh7th/cmp-path", -- source for file system paths
+      "hrsh7th/cmp-buffer", -- source for text in buffer
+      "hrsh7th/cmp-nvim-lsp", -- source for snippets from the lsp
+      {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+          "saadparwaiz1/cmp_luasnip",
+          "rafamadriz/friendly-snippets"
+        }
+      },
+    },
     config = function()
       local cmp = require("cmp")
       require("luasnip.loaders.from_vscode").lazy_load()
@@ -27,15 +29,20 @@ return {
           documentation = cmp.config.window.bordered()
         },
         mapping = cmp.mapping.preset.insert({
+          -- ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+          -- ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select=true })
         }),
+        -- sources for autocomplition
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" },
+          { name = "luasnip" }, -- snippets
+          { name = "path" }, -- file system paths
+          { name = "buffer" }, -- text within current buffer
         },
         {
           { name = "buffer" },
